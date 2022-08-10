6785f08377d20957b093f7df8750d29cb658be03
dev-nasreen <nasreen.akter34@gmail.com>
2022-08-10 15:04:51 +0600

    reasons images done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/hero/Hero';
 import Programs from './components/programs/Programs';
+import Reasons from './components/reasons/Reasons';
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons />
     </div>
   );
 }
