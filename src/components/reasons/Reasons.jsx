703487aacec878c35f78f6d5b5657ca92d2d832a
dev-nasreen <nasreen.akter34@gmail.com>
2022-08-10 16:00:37 +0600

    reasons section done

--- src/components/reasons/Reasons.jsx
+++ src/components/reasons/Reasons.jsx
@@ -4,6 +4,10 @@ import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
+import tick from '../../assets/tick.png'
+import nike from '../../assets/nike.png';
+import nb from '../../assets/nb.png';
+import adidas from '../../assets/adidas.png';
 const Reasons = () => {
   return (
     <div className="Reasons" id="Reasons">
@@ -14,7 +18,35 @@ const Reasons = () => {
         <img src={image4} alt="" />
       </div>
       <div className="right-r">
-       right side
+       <span>Some Reasons</span>
+       <div>
+        <span>Why</span> <span>choose us</span>
+       </div>
+       <div className="details-r">
+        <div>
+            <img src={tick} alt="fitclub"/>
+            <span>over 140+ expert coachs</span>
+        </div>
+        <div>
+            <img src={tick} alt="fitclub"/>
+            <span>train smarter and faster than before</span>
+        </div>
+        <div>
+            <img src={tick} alt="fitclub"/>
+            <span>1 free program for new member</span>
+        </div>
+        <div>
+            <img src={tick} alt="fitclub"/>
+            <span>reliable partners</span>
+        </div>
+       </div>
+       <span style={{color:"var(--gray)", fontWeight:"normal"}}>our partners</span>
+       <div className="partner">
+         <img src={nb} alt=""/>
+         <img src={nike} alt=""/>
+         <img src={adidas} alt=""/>
+        
+       </div>
       </div>
     </div>
   );
