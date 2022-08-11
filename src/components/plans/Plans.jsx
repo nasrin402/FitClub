822609de550a1ac57ff5eff33d9df1d202d8f96b
dev-nasreen <nasreen.akter34@gmail.com>
2022-08-11 18:56:10 +0600

    plans section done

--- src/components/plans/Plans.jsx
+++ src/components/plans/Plans.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Plans.css';
 import {plansData} from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
 const Plans = () => {
     return (
         <div className='plans_container'>
@@ -11,9 +12,20 @@ const Plans = () => {
            </div>
            <div className='plans'>
                 {plansData.map((plan, i) =>(
-                    <div className='s_plan' key={i}>
+                    <div className='plan' key={i}>
                         {plan.icon}
                         <span>{plan.title}</span>
+                        <span>$ {plan.price}</span>
+                        <div className='features'>
+                            {plan.features.map((feature, i)=>(<div className='feature' key={i}>
+                               <img src={whiteTick} alt=""/>
+                            <span>{feature}</span> 
+                                </div>))}
+                        </div>
+                        <div>
+                            <span>See more benefits -> </span>
+                        </div>
+                        <button className="btn" >Join now</button>
                     </div>
                 ))}
            </div>
